de70734f87b3e3fc86220d03edc4f92eb5aaa0f8
siggmar <subberman0822@gmail.com>
2025-11-15 17:53:12 +0100

    added moving gradient

--- ws2812/ws2812.c
+++ ws2812/ws2812.c
@@ -1,6 +1,7 @@
 #include "hardware/pio.h"
 #include "pico/stdlib.h"
 #include "ws2812.pio.h"
+#include <math.h>
 #include <stdio.h>
 
 #define LED_PIN 0
@@ -28,15 +29,21 @@ void lights_off() {
 }
 
 void gradiant(uint8_t r_start, uint8_t g_start, uint8_t b_start, uint8_t r_end,
-              uint8_t g_end, uint8_t b_end, float brightness) {
+              uint8_t g_end, uint8_t b_end, float brightness, float t) {
     float distance_r1 = (r_end - r_start) / (float)NUM_PIXELS;
     float distance_g1 = (g_end - g_start) / (float)NUM_PIXELS;
     float distance_b1 = (b_end - b_start) / (float)NUM_PIXELS;
 
-    for (int i = 0; i < NUM_PIXELS; i++) { // blue to purple
-        put_pixel(urgb_u32(scale(brightness, r_start + (distance_r1 * i)),
-                           scale(brightness, g_start + (distance_g1 * i)),
-                           scale(brightness, b_start + (distance_b1 * i))));
+    float offset = fmodf(t, NUM_PIXELS);
+
+    for (int i = 0; i < NUM_PIXELS; i++) {
+        float pos = i + offset;
+        if (pos >= NUM_PIXELS) {
+            pos -= NUM_PIXELS;
+        }
+        put_pixel(urgb_u32(scale(brightness, r_start + (distance_r1 * pos)),
+                           scale(brightness, g_start + (distance_g1 * pos)),
+                           scale(brightness, b_start + (distance_b1 * pos))));
     }
 }
 
@@ -50,7 +57,7 @@ int main() {
     uint offset = pio_add_program(pio, &ws2812_program);
     ws2812_program_init(pio, sm, offset, LED_PIN, 800000, IS_RGBW);
 
-    float brightness = 0.1f;
+    float brightness = 1.0f;
 
     uint8_t r1 = 0, g1 = 15, b1 = 145;
     uint8_t r2 = 181, g2 = 0, b2 = 201;
@@ -58,43 +65,23 @@ int main() {
     // dark blue 0, 15, 145
     // purple 181, 0, 201
 
-    uint8_t r3 = 255, g3 = 0, b3 = 0;
-    uint8_t r4 = 255, g4 = 0, b4 = 144;
-
-    // red 255, 0, 0
-    // pink 255, 0, 144
-
     bool touch = 0;
-    bool color_switch = false;
-    int timeout = 0;
+    bool off_switch = true;
+    int t = 0;
 
     while (true) {
         touch = gpio_get(TOUCH_PIN);
+        sleep_ms(50);
         if (touch) {
-            color_switch = !color_switch;
-            printf("Colors switched \n");
+            off_switch = !off_switch;
+            printf("llllllllllllllllllllllllllllllllllllllllllllllllllllll \n");
+        }
+        if (!off_switch) {
+            gradiant(r1, g1, b1, r2, g2, b2, brightness, t);
         } else {
-            timeout++;
-        }
-        if (timeout == 150) { // turn led off after 30s
-            timeout = 0;
             lights_off();
-            while (!gpio_get(TOUCH_PIN)) {
-                printf("paused \n");
-            };
-            // pause programm
         }
-
-        if (color_switch) {
-            for (int i = 0; i < NUM_PIXELS; i++) { // blue to purple
-                gradiant(r1, g1, b1, r2, g2, b2, brightness);
-            }
-        }
-        if (gpio_get(TOUCH_PIN)) { // red to pink
-            for (int i = 0; i < NUM_PIXELS; i++) {
-                gradiant(r3, g3, b3, r4, g4, b4, brightness);
-            }
-        }
-        sleep_ms(200);
+        t++;
+        sleep_ms(50);
     }
 }
